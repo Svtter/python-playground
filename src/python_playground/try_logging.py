9c715567500e8d512d3e4b4d04ca7e46836394b0
svtter <svtter@qq.com>
2024-06-01 10:38:26 +0800

    initialized commit

---
+++ src/python_playground/try_logging.py
@@ -0,0 +1,21 @@
+import logging
+
+# create logger with 'spam_application'
+logger = logging.getLogger('trainner')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('train.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+logger.info('test')
